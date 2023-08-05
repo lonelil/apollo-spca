0acece6b762d9293b7e121ff5e943aa9fe103f86
lonelil <51315646+lonelil@users.noreply.github.com>
2023-08-06 03:09:16 +0800

    nvm

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    //unoptimized: true,
-    loader: "custom",
-    loaderFile: "./src/lib/imageLoader.ts"
+    unoptimized: true,
+    //loader: "custom",
+    //loaderFile: "./src/lib/imageLoader.ts"
   },
   output: "export",
 };
